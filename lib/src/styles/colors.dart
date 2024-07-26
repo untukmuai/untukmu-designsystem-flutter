154cc311a377c3548ad17cb26855945f4d04e63c
Mochammad Yusuf Fachroni <yusuf.fachroni@gmail.com>
2024-07-26 09:36:09 +0700

    init

---
+++ lib/src/styles/colors.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+class DLSColors {
+  // Core Colors
+  static const Color orchidPurpleDarker = Color(0xFF2B1664);
+  static const Color orchidPurpleDark = Color(0xFF5A36BF);
+  static const Color orchidPurpleBase = Color(0xFF6E3FF3);
+  static const Color orchidPurpleLight = Color(0xFFCAC2FF);
+  static const Color orchidPurpleLighter = Color(0xFFEEEBFF);
+
+  // Secondary Colors
+  static const Color sunsetYellowDarker = Color(0xFF6E330C);
+  static const Color sunsetYellowDark = Color(0xFFC2540A);
+  static const Color sunsetYellowBase = Color(0xFFF17B2C);
+  static const Color sunsetYellowLight = Color(0xFFFFDAC2);
+  static const Color sunsetYellowLighter = Color(0xFFFEF3EB);
+
+  static const Color flamingoPinkDarker = Color(0xFF620F6C);
+  static const Color flamingoPinkDark = Color(0xFF9C23A9);
+  static const Color flamingoPinkBase = Color(0xFFE255F2);
+  static const Color flamingoPinkLight = Color(0xFFF9C2FF);
+  static const Color flamingoPinkLighter = Color(0xFFFDEBFF);
+
+  static const Color pacificBlueDarker = Color(0xFF162664);
+  static const Color pacificBlueDark = Color(0xFF253EA7);
+  static const Color pacificBlueBase = Color(0xFF375DFB);
+  static const Color pacificBlueLight = Color(0xFFC2D6FF);
+  static const Color pacificBlueLighter = Color(0xFFEBF1FF);
+
+  static const Color basilGreenDarker = Color(0xFF176448);
+  static const Color basilGreenDark = Color(0xFF2D9F75);
+  static const Color basilGreenBase = Color(0xFF38C793);
+  static const Color basilGreenLight = Color(0xFFCBF5E5);
+  static const Color basilGreenLighter = Color(0xFFEFFAF6);
+
+  // Neutral Colors
+  static const Color neutral900 = Color(0xFF0A0D14);
+  static const Color neutral800 = Color(0xFF161922);
+  static const Color neutral700 = Color(0xFF20232D);
+  static const Color neutral600 = Color(0xFF31353F);
+  static const Color neutral500 = Color(0xFF525866);
+  static const Color neutral400 = Color(0xFF868C98);
+  static const Color neutral300 = Color(0xFFCDD0D5);
+  static const Color neutral200 = Color(0xFFE2E4E9);
+  static const Color neutral100 = Color(0xFFF6F8FA);
+  static const Color neutral0 = Color(0xFFFFFFFF);
+
+  // Semantic Colors
+  static const Color redDarker = Color(0xFF710E21);
+  static const Color redDark = Color(0xFFAF1D38);
+  static const Color redBase = Color(0xFFDFFC41);
+  static const Color redLight = Color(0xFFF8C9D2);
+  static const Color redLighter = Color(0xFFFDEDF0);
+
+  static const Color yellowDarker = Color(0xFF693D11);
+  static const Color yellowDark = Color(0xFFB47818);
+  static const Color yellowBase = Color(0xFFF2AE40);
+  static const Color yellowLight = Color(0xFFFBDFB1);
+  static const Color yellowLighter = Color(0xFFFEF7EC);
+
+  static const Color tealDarker = Color(0xFF164564);
+  static const Color tealDark = Color(0xFF1F87AD);
+  static const Color tealBase = Color(0xFF35B9E9);
+  static const Color tealLight = Color(0xFFC2EFFF);
+  static const Color tealLighter = Color(0xFFEBFAFF);
+
+  // Additional Colors from MAIN COLOURS section
+  static const Color primaryDarker = orchidPurpleDarker;
+  static const Color primaryDark = orchidPurpleDark;
+  static const Color primaryBase = orchidPurpleBase;
+  static const Color primaryLight = orchidPurpleLight;
+  static const Color primaryLighter = orchidPurpleLighter;
+
+  static const Color secondaryDarker = sunsetYellowDarker;
+  static const Color secondaryDark = sunsetYellowDark;
+  static const Color secondaryBase = sunsetYellowBase;
+  static const Color secondaryLight = sunsetYellowLight;
+  static const Color secondaryLighter = sunsetYellowLighter;
+
+  static const Color tertiaryDarker = flamingoPinkDarker;
+  static const Color tertiaryDark = flamingoPinkDark;
+  static const Color tertiaryBase = flamingoPinkBase;
+  static const Color tertiaryLight = flamingoPinkLight;
+  static const Color tertiaryLighter = flamingoPinkLighter;
+
+  static const Color quaternaryDarker = pacificBlueDarker;
+  static const Color quaternaryDark = pacificBlueDark;
+  static const Color quaternaryBase = pacificBlueBase;
+  static const Color quaternaryLight = pacificBlueLight;
+  static const Color quaternaryLighter = pacificBlueLighter;
+
+  static const Color quinaryDarker = basilGreenDarker;
+  static const Color quinaryDark = basilGreenDark;
+  static const Color quinaryBase = basilGreenBase;
+  static const Color quinaryLight = basilGreenLight;
+  static const Color quinaryLighter = basilGreenLighter;
+
+  // Background Color Tokens
+  static const Color bgStrong900 = neutral900;
+  static const Color bgSurface700 = neutral700;
+  static const Color bgSoft200 = neutral200;
+  static const Color bgWeak100 = neutral100;
+
+  // Text Color Tokens
+  static const Color textMain900 = neutral900;
+  static const Color textSub500 = neutral500;
+  static const Color textSoft400 = neutral400;
+  static const Color textDisabled300 = neutral300;
+  static const Color textWhite0 = neutral0;
+
+  // Stroke Color Tokens
+  static const Color strokeStrong900 = neutral900;
+  static const Color strokeSub300 = neutral300;
+  static const Color strokeSoft200 = neutral200;
+  static const Color strokeDisabled100 = neutral100;
+  static const Color strokeWhite0 = neutral0;
+
+  // Icon Color Tokens
+  static const Color iconStrong900 = neutral900;
+  static const Color iconSub500 = neutral500;
+  static const Color iconSoft400 = neutral400;
+  static const Color iconDisabled300 = neutral300;
+  static const Color iconWhite0 = neutral0;
+
+  // State Colors
+  static const Color fadedDark = neutral800;
+  static const Color fadedBase = neutral500;
+  static const Color fadedLight = neutral200;
+  static const Color fadedLighter = neutral100;
+
+  static const Color informationDark = pacificBlueDarker;
+  static const Color informationBase = pacificBlueBase;
+  static const Color informationLight = pacificBlueLight;
+  static const Color informationLighter = pacificBlueLighter;
+
+  static const Color warningDark = sunsetYellowDarker;
+  static const Color warningBase = sunsetYellowBase;
+  static const Color warningLight = sunsetYellowLight;
+  static const Color warningLighter = sunsetYellowLighter;
+
+  static const Color errorDark = redDarker;
+  static const Color errorBase = redBase;
+  static const Color errorLight = redLight;
+  static const Color errorLighter = redLighter;
+
+  static const Color successDark = basilGreenDarker;
+  static const Color successBase = basilGreenBase;
+  static const Color successLight = basilGreenLight;
+  static const Color successLighter = basilGreenLighter;
+
+  static const Color verifiedDark = tealDarker;
+  static const Color verifiedBase = tealBase;
+  static const Color verifiedLight = tealLight;
+  static const Color verifiedLighter = tealLighter;
+}
---
+++ lib/untukmu_flutter_design_system.dart
@@ -0,0 +1,3 @@
+library untukmu_flutter_design_system;
+
+export 'src/styles/colors.dart';
