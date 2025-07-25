77eb95da9995ca482a8262708ff66173a7bc7967
Feriyal Susanto <feriyalsusanto@gmail.com>
2025-07-25 13:45:42 +0700

    improving dropdown dynamic value

--- lib/src/components/dropdown/custom_dropdown_widget.dart
+++ lib/src/components/dropdown/custom_dropdown_widget.dart
@@ -5,19 +5,24 @@ import 'package:untukmu_flutter_design_system/untukmu_flutter_design_system.dart
 
 enum DropdownLabelDirection { vertical, horizontal }
 
-class CustomDropdownWidget extends StatefulWidget {
+class CustomDropdownWidget<T> extends StatefulWidget {
   final String label;
   final String hintText;
   final bool isEditable;
   final bool isInvalid;
   final bool showOptionalLabel;
+  final bool showOptionalIcon;
+  final bool isRequired;
   final DropdownLabelDirection labelDirection;
   final String? hintTextMessage;
 
   final Widget? prefixIcon;
-  final List<String> items;
+  final List<T> items;
+  final String Function(T)?
+      itemLabelBuilder; // Function to get display text from item
+  final T? value; // Currently selected value
 
-  final ValueChanged<String?>? onChanged;
+  final ValueChanged<T?>? onChanged;
 
   const CustomDropdownWidget({
     super.key,
@@ -25,19 +30,23 @@ class CustomDropdownWidget extends StatefulWidget {
     required this.hintText,
     this.isEditable = true,
     this.showOptionalLabel = false,
+    this.showOptionalIcon = false,
+    this.isRequired = false,
     this.labelDirection = DropdownLabelDirection.vertical,
     this.isInvalid = false,
     this.hintTextMessage,
     this.prefixIcon,
     required this.items,
+    this.itemLabelBuilder,
+    this.value,
     this.onChanged,
   });
 
   @override
-  CustomDropdownWidgetState createState() => CustomDropdownWidgetState();
+  CustomDropdownWidgetState<T> createState() => CustomDropdownWidgetState<T>();
 }
 
-class CustomDropdownWidgetState extends State<CustomDropdownWidget> {
+class CustomDropdownWidgetState<T> extends State<CustomDropdownWidget<T>> {
   @override
   Widget build(BuildContext context) {
     return widget.labelDirection == DropdownLabelDirection.vertical
@@ -50,12 +59,20 @@ class CustomDropdownWidgetState extends State<CustomDropdownWidget> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
+          spacing: 4,
           children: [
             LabelWidget(label: widget.label),
-            if (widget.showOptionalLabel) const SizedBox(width: 4),
             if (widget.showOptionalLabel)
+              LabelWidget(
+                label: "Optional",
+                labelType: LabelType.optional,
+                showOptionalIcon: widget.showOptionalIcon,
+              ),
+            if (widget.isRequired)
               const LabelWidget(
-                  label: "Optional", labelType: LabelType.optional),
+                label: "Mandatory",
+                labelType: LabelType.mandatory,
+              ),
           ],
         ),
         const SizedBox(height: 8),
@@ -63,8 +80,9 @@ class CustomDropdownWidgetState extends State<CustomDropdownWidget> {
         const SizedBox(height: 4),
         if (widget.hintTextMessage != null)
           HintTextWidget(
-              hint: widget.hintTextMessage!,
-              hintState: widget.isInvalid ? HintState.error : HintState.normal),
+            hint: widget.hintTextMessage!,
+            hintState: widget.isInvalid ? HintState.error : HintState.normal,
+          ),
       ],
     );
   }
@@ -79,39 +97,47 @@ class CustomDropdownWidgetState extends State<CustomDropdownWidget> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
+                spacing: 4,
                 children: [
                   LabelWidget(label: widget.label),
-                  if (widget.showOptionalLabel) const SizedBox(width: 4),
                   if (widget.showOptionalLabel)
+                    LabelWidget(
+                      label: "Optional",
+                      labelType: LabelType.optional,
+                      showOptionalIcon: widget.showOptionalIcon,
+                    ),
+                  if (widget.isRequired)
                     const LabelWidget(
-                        label: "Optional", labelType: LabelType.optional),
+                      label: "Mandatory",
+                      labelType: LabelType.mandatory,
+                    ),
                 ],
               ),
               const SizedBox(height: 16),
               if (widget.hintTextMessage != null)
                 HintTextWidget(
-                    hint: widget.hintTextMessage!,
-                    hintState:
-                        widget.isInvalid ? HintState.error : HintState.normal),
+                  hint: widget.hintTextMessage!,
+                  hintState:
+                      widget.isInvalid ? HintState.error : HintState.normal,
+                ),
             ],
           ),
         ),
         const SizedBox(width: 16),
-        Expanded(
-          flex: 3,
-          child: _buildDropdown(),
-        ),
+        Expanded(flex: 3, child: _buildDropdown()),
       ],
     );
   }
 
   Widget _buildDropdown() {
-    return DropdownButtonFormField<String?>(
-      items: widget.items.map(
-        (e) {
-          return DropdownMenuItem(value: e, child: Text(e));
-        },
-      ).toList(),
+    return DropdownButtonFormField<T?>(
+      value: widget.value,
+      items: widget.items.map((item) {
+        return DropdownMenuItem<T?>(
+          value: item,
+          child: Text(_getItemLabel(item)),
+        );
+      }).toList(),
       hint: Text(
         widget.hintText,
         style: DLSTextStyle.labelSmall.copyWith(color: DLSColors.textSoft400),
@@ -137,13 +163,16 @@ class CustomDropdownWidgetState extends State<CustomDropdownWidget> {
         contentPadding: const EdgeInsets.all(DLSSizing.s2xSmall),
         border: border,
         errorBorder: border.copyWith(
-            borderSide: const BorderSide(color: DLSColors.redBase)),
+          borderSide: const BorderSide(color: DLSColors.redBase),
+        ),
         enabledBorder: border,
         focusedBorder: border.copyWith(
-            borderSide: const BorderSide(color: DLSColors.primaryBase)),
+          borderSide: const BorderSide(color: DLSColors.primaryBase),
+        ),
         disabledBorder: InputBorder.none,
         focusedErrorBorder: border.copyWith(
-            borderSide: const BorderSide(color: DLSColors.errorBase)),
+          borderSide: const BorderSide(color: DLSColors.errorBase),
+        ),
         fillColor: widget.isEditable ? DLSColors.bgWhite0 : DLSColors.bgWeak100,
         filled: true,
       ),
@@ -157,7 +186,15 @@ class CustomDropdownWidgetState extends State<CustomDropdownWidget> {
     );
   }
 
+  String _getItemLabel(T item) {
+    if (widget.itemLabelBuilder != null) {
+      return widget.itemLabelBuilder!(item);
+    }
+    return item.toString();
+  }
+
   InputBorder get border => OutlineInputBorder(
-      borderRadius: DLSRadius.radius12,
-      borderSide: const BorderSide(color: DLSColors.strokeSoft200));
+        borderRadius: DLSRadius.radius12,
+        borderSide: const BorderSide(color: DLSColors.strokeSoft200),
+      );
 }
